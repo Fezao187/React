cd65f705cb41240907e42c492f3f06f6ad5ab2d7
Katlego Maredi <katlegomaredi9@gmail.com>
2024-05-01 15:07:09 +0200

    Chapter 2: Creating and using components

---
+++ chapter_02/new_project/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Products from './Products';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          My First React App!
+          <Products/>
+        </h1>
+      </div>
+    );
+  }
+}
+export default App;
