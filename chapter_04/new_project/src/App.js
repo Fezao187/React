6357385d7df81d00e786e3fb12ba844c3033404f
Katlego Maredi <katlegomaredi9@gmail.com>
2024-05-03 10:09:47 +0200

    Chapter 4: Working with components

---
+++ chapter_04/new_project/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Products from './Products';
+import { Button } from 'react-bootstrap';
+// import Rating from './Rating';
+class App extends Component {
+  render() {
+    const isValid = true;
+    return (
+      <div className='App'>
+        <h1>
+          My First React App!
+        </h1>
+        <Products />
+        <Button variant="outline-primary" disabled={!isValid}>Default</Button>
+        {/* <Rating rating="1" />
+        <Rating rating="2" />
+        <Rating rating="3" />
+        <Rating rating="4" />
+        <Rating rating="5" /> */}
+      </div>
+    );
+  }
+}
+export default App;
