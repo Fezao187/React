03a626806e16708f6b644bb469eef88f51ed7e9a
Katlego Maredi <katlegomaredi9@gmail.com>
2024-05-06 17:32:00 +0200

    CHAPTER 7: GETTING DATA FROM RESTFUL APIS WITH AXIOS

---
+++ chapter_07/new_project/src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import Products from './Products';
+import { Button } from 'react-bootstrap';
+import UserForm from './UserForm';
+import GitHub from './GitHub';
+// Import jumbotron
+import JumboTronComponent from './JumboTronComponent';
+// import Rating from './Rating';
+class App extends Component {
+  render() {
+    const isValid = true;
+    return (
+      <div className='App'>
+        <JumboTronComponent >
+          This is a long sentence, and I want to insert content into the
+          jumbotron component from the outside.
+        </JumboTronComponent>
+        <h1>
+          My First React App!
+        </h1>
+        <Products />
+        <Button variant="outline-primary" disabled={!isValid}>Default</Button>
+        {/* <Rating rating="1" />
+        <Rating rating="2" />
+        <Rating rating="3" />
+        <Rating rating="4" />
+        <Rating rating="5" /> */}
+        <UserForm/>
+        <br></br>
+        <GitHub /> 
+        <br></br>
+      </div>
+    );
+  }
+}
+export default App;
