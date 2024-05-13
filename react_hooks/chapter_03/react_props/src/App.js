06368046b380f1273fce73329699a31386efbdeb
Fezao187 <katlegomaredi9@gmail.com>
2024-05-14 01:46:30 +0200

    Chapter 3: Binding, events, props and state

---
+++ react_hooks/chapter_03/react_props/src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Products from './Products';
+import { Button } from 'react-bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import Rating from './Rating';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+function App() {
+  const user = {
+    firstName: 'Greg',
+    lastName: 'Lim',
+    imageUrl: 'https://picsum.photos/200/300'
+  };
+
+  const isValid = true;
+  return (
+    <div>
+      <h1>Hello, {formatName(user)}</h1>
+      <br />
+      <img src={user.imageUrl} alt='random_img'></img>
+      {/* Call the products component */}
+      <hr />
+      <Products />
+      <Button variant="outline-primary" disabled={!isValid}>Default</Button>
+      <hr />
+      <Rating rating='1' />
+      <Rating rating='2' />
+      <Rating rating='3' />
+      <Rating rating='4' />
+      <Rating rating='5' />
+    </div>
+  );
+}
+export default App;
