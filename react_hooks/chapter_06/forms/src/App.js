1f2908a745bffb80cc70b2ee4ef318f7900a01aa
Fezao187 <katlegomaredi9@gmail.com>
2024-05-16 00:18:31 +0200

    Chapter 6: Creating forms with hooks

---
+++ react_hooks/chapter_06/forms/src/App.js
@@ -0,0 +1,13 @@
+import UserForm from './UserForm';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div>
+      <UserForm/>
+    </div>
+  );
+}
+
+export default App;
