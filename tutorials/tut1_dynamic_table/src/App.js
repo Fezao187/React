6c50f5ef2100c15f9f82a8dbfe41b2e04a30a54a
Katlego Maredi <katlegomaredi9@gmail.com>
2024-05-17 00:49:22 +0200

    Tutorial 1: Dynamic table

---
+++ tutorials/tut1_dynamic_table/src/App.js
@@ -0,0 +1,9 @@
+// Render Dynamic Table Component
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import DynamicTable from "./dynamic_table/DynamicTable";
+function App() {
+    return (
+      <DynamicTable />
+    );
+}
+export default App;
