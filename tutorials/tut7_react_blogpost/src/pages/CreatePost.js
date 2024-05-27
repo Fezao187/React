915711f8181369826997d733ad6012735d25af2b
Fezao187 <katlegomaredi9@gmail.com>
2024-05-27 16:24:02 +0200

    Tuturial 7: React blogpost with firebase

--- tutorials/tut7_react_blogpost/src/pages/CreatePost.js
+++ tutorials/tut7_react_blogpost/src/pages/CreatePost.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../firebase_config";
+import { useNavigate } from "react-router-dom";
+function CreatePost({isAuth}) {
+    const [title, setTitle] = useState("");
+    const [postText, setPostText] = useState("");
 
-function CreatePost() {
+    const postsCollectionRef = collection(db, "posts");
+    let navigate = useNavigate();
+    const createPost = async () => {
+        await addDoc(postsCollectionRef, {
+            title,
+            postText,
+            auther: {
+                name: auth.currentUser.displayName,
+                id: auth.currentUser.uid
+            }
+        });
+        navigate("/");
+    }
+
+    useEffect(()=>{
+        if(!isAuth){
+            navigate("/login");
+        }
+    })
     return (
         <div className="createPostPage">
             <div className="cpContainer">
                 <h1>Create a post</h1>
                 <div className="inputGp">
                     <label>Title:</label>
-                    <input placeholder="Title....." />
+                    <input placeholder="Title....." onChange={(event) => {
+                        setTitle(event.target.value);
+                    }} />
                 </div>
                 <div className="inputGp">
-                <label>Post:</label>
-                <textarea placeholder="Post....."/>
+                    <label>Post:</label>
+                    <textarea placeholder="Post....." onChange={(event) => {
+                        setPostText(event.target.value);
+                    }} />
                 </div>
-                <button>Submit Post</button>
+                <button onClick={createPost}>Submit Post</button>
             </div>
         </div>
     );
